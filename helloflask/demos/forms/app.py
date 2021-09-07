62d405589745409e710b126b7366a0b24b17050b
zhaofangfang1991 <helloworldzff@163.com>
2021-09-07 10:36:17 +0800

    templates

--- helloflask/demos/forms/app.py
+++ helloflask/demos/forms/app.py
@@ -1,5 +1,45 @@
-from flask import Flask
+from flask import Flask,render_template,request,flash,redirect,url_for
 import os
+from forms import LoginForm
 
 app = Flask(__name__)
 app.secret_key = os.getenv('SECRET_KEY', 'default_string')
+app.config['WTF_I18N_ENABLED'] = False # 让flask-wtf使用wtforms内置的错误消息翻译
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template('index.html')
+
+
+# 1、在视图函数中实例化表单类LoginForm   2、在render_template()中使用关键字from将表单实例传入模板
+'''
+首先是实例化表单；如果是GET请求，就渲染模板；如果是POST请求，就调用validate()验证表单数据；
+'''
+@app.route('/basic', methods=['GET', 'POST'])
+def basic():
+    form = LoginForm()
+    ''' 第一种写法
+    if request.method == 'POST' and form.validate():
+        pass # 获取表单数据并保存'''
+    if form.validate_on_submit(): # 第二种写法
+        # pass # 获取表单数据并保存
+        username = form.username.data
+        flash('欢迎你，%s' %username)
+        return redirect(url_for('index'))
+    return render_template('basic.html', form=form)
+
+@app.route('/bootstrap')
+def bootstrap():
+    form = LoginForm()
+    return render_template('bootstrap.html', form=form)
+
+
+
+
+
+@app.route('/hong', methods=['POST', 'GET'])
+def hong():
+    form = LoginForm()
+    if form.validate_on_submit():
+        pass # 校验数据，并重定向
+    return render_template('hong.html', form=form)
--- helloflask/demos/forms/forms.py
+++ helloflask/demos/forms/forms.py
@@ -7,4 +7,17 @@ from wtforms.validators import DataRequired,Length
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()]) # <input type='text' name='Username' required>
     password = PasswordField('Password', validators=[DataRequired(), Length(8,128)]) # <input type='password' name='Password' required length=[8,128]>
-    remember = BooleanField('Remember me') # <radio >
+    remember = BooleanField('Remember me') # <input type='radio' name='Remember me' >
+    submit = SubmitField('Log in') # <input type='submit' value='log in'>
+
+# 表单类的基类
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length
+class MyBaseForm(FlaskForm):
+    class Meta:
+        locales = ['zh']
+
+class HelloForm(MyBaseForm):
+    name = StringField('Name', validators=[DataRequired()])
+    submit = SubmitField()
